ad10a855a6496a54742661244e4cb5f4588f1544
Brian <brian.sila1@student.moringaschool.com>
2025-05-23 22:20:38 +0300

    Theatre lab

---
+++ lib/alembic/versions/a955edc86f79_fixed_relationships_and_typos.py
@@ -0,0 +1,24 @@
+"""fixed relationships and typos
+
+Revision ID: a955edc86f79
+Revises: c17aa1d7d7a9
+Create Date: 2025-05-23 22:16:40.581417
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a955edc86f79'
+down_revision = 'c17aa1d7d7a9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ lib/alembic/versions/c17aa1d7d7a9_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: c17aa1d7d7a9
+Revises: 
+Create Date: 2025-05-23 22:09:36.585763
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c17aa1d7d7a9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
